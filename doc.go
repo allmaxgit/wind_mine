fd4b86df7cddbf876a5c6a77d31274bbd4f897ac
Lev Sizov <lsizov@allmax.team>
2018-03-22 13:43:40 +0300

    Add new json bindings, fixed wrong package name
    for generated UsingFiatPrice binding

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 //go:generate abigen --sol ./contracts/Crowdsale.sol --pkg token  --out ./gocontracts/Crowdsale.go
-//go:generate abigen --sol ./contracts/UsingFiatPrice.sol --pkg token  --out ./services/ExchangeRateService/contract.go
+//go:generate abigen --sol ./contracts/UsingFiatPrice.sol --pkg main  --out ./services/ExchangeRateService/contract.go
 
 //go:generate protoc -I/usr/local/include -I.   -I$GOPATH/src   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis   --go_out=plugins=grpc:.                 api/buyer/buyer.proto
 //go:generate protoc -I/usr/local/include -I.   -I$GOPATH/src   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis   --grpc-gateway_out=logtostderr=true:.   api/buyer/buyer.proto
